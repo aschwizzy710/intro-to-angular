c475c07f1fc31075e3efa69f1ff51fc9a6e89901
Aaron Schwartz <aschwartz628@gmail.com>
2016-10-25 18:48:58 -0400

    added the ability to edit and save the edits for individual todos in the application

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -8,6 +8,8 @@ function MainController($scope, TodoService){ // $scope is our bridge to the dom
   $scope.todos = TodoService.get();
   $scope.createTodo = createTodo;
   $scope.deleteTodo = deleteTodo;
+  $scope.editTodo = editTodo;
+  $scope.saveTodo = saveTodo;
 
   function createTodo(newTodo){
     TodoService.create(newTodo);
@@ -16,6 +18,13 @@ function MainController($scope, TodoService){ // $scope is our bridge to the dom
 
   function deleteTodo(index){
     TodoService.delete(index);
+  }
+  function editTodo(todo){
+    todo.isBeingEdited = true;
+  }
+  function saveTodo(index, todo){
+    TodoService.update(index, todo.desc);
+    todo.isBeingEdited = false;
   }
 }
 }());
