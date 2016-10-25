5af658b4463bad895cfce04761e37deb88306040
Aaron Schwartz <aschwartz628@gmail.com>
2016-10-24 20:50:20 -0400

    added the ability to remove todos

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -7,10 +7,15 @@ MainController.$inject = ['$scope', 'TodoService']; // what tools the MainContro
 function MainController($scope, TodoService){ // $scope is our bridge to the dom
   $scope.todos = TodoService.get();
   $scope.createTodo = createTodo;
+  $scope.deleteTodo = deleteTodo;
 
   function createTodo(newTodo){
     TodoService.create(newTodo);
     $scope.newTodo = '';
+  }
+
+  function deleteTodo(index){
+    TodoService.delete(index);
   }
 }
 }());
